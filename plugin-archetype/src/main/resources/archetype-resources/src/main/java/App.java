7348761ffee4a2f58b67d6766f6d634453ed909b
Peter Withers <Peter.Withers@mpi.nl>
2012-11-14 09:34:58 +0000

    Created a maven archtype based on the sample plugin.

---
+++ plugin-archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $nl.mpi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
