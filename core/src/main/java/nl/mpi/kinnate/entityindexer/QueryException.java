ee2d41c384ac7f444c23e0a914fce3c6817aea1a
Peter Withers <Peter.Withers@mpi.nl>
2012-11-29 12:37:11 +0000

    Corrected the java docs. This commit is to test why svn is giving different revision numbers on mac vs linux.

--- core/src/main/java/nl/mpi/kinnate/entityindexer/QueryException.java
+++ core/src/main/java/nl/mpi/kinnate/entityindexer/QueryException.java
@@ -1,9 +1,9 @@
 package nl.mpi.kinnate.entityindexer;
 
 /**
- * Document : QueryException
  * Created on : Jul 4, 2012, 4:23:37 PM
- * Author : Peter Withers
+ *
+ * @author Peter Withers
  */
 public class QueryException extends Exception {
 
