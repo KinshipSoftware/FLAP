31783e0655b0e5a51c6e221f8093843ce5e1523e
Twan Goosen <Twan.Goosen@mpi.nl>
2013-01-17 13:52:37 +0000

    Added additional constructors to PluginException so that it can also have inner exceptions

--- core/src/main/java/nl/mpi/arbil/plugin/PluginException.java
+++ core/src/main/java/nl/mpi/arbil/plugin/PluginException.java
@@ -8,12 +8,12 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.arbil.plugin;
 
@@ -25,6 +25,14 @@ package nl.mpi.arbil.plugin;
 public class PluginException extends Exception {
 
     public PluginException(String string) {
-        super(string);
+	super(string);
+    }
+
+    public PluginException(Throwable cause) {
+	super(cause);
+    }
+
+    public PluginException(String message, Throwable cause) {
+	super(message, cause);
     }
 }
