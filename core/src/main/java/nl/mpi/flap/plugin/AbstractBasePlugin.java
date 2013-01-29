14173275a66f32872cf2d3b31825478fd244ca7e
Twan Goosen <Twan.Goosen@mpi.nl>
2013-01-29 15:45:57 +0000

    Writing to stdout instead of logging debug level in isMavenVersionCorrect(). Instead logging in constructor when properties actually get read

--- core/src/main/java/nl/mpi/flap/plugin/AbstractBasePlugin.java
+++ core/src/main/java/nl/mpi/flap/plugin/AbstractBasePlugin.java
@@ -8,12 +8,12 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package nl.mpi.flap.plugin;
 
@@ -37,84 +37,96 @@ public abstract class AbstractBasePlugin implements BasePlugin {
     final private String compileDateString;
     final private String artifactVersionString;
     final private String lastCommitDate;
-    
-    private final static Logger logger = LoggerFactory.getLogger(AbstractBasePlugin.class);
+    final private Logger logger = LoggerFactory.getLogger(getClass());
 
     public AbstractBasePlugin(String nameString, String descriptionString, String packageString) throws PluginException {
-        this.nameString = nameString;
-        this.descriptionString = descriptionString;
+	this.nameString = nameString;
+	this.descriptionString = descriptionString;
 
-        Properties properties = new Properties();
-        try {
-            String propertiesPath = packageString.replace(".", "/");
-            properties.load(getClass().getResourceAsStream("/" + propertiesPath + "/version.properties"));
-            majorVersionNumber = Integer.parseInt(properties.getProperty("plugin.majorVersion"));
-            minorVersionNumber = Integer.parseInt(properties.getProperty("plugin.minorVersion"));
-            buildVersionNumber = Integer.parseInt(properties.getProperty("plugin.buildVersion"));
-            lastCommitDate = properties.getProperty("plugin.lastCommitDate");
-            compileDateString = properties.getProperty("plugin.compileDate");
-            artifactVersionString = properties.getProperty("plugin.projectVersion");
-        } catch (IOException ex) {
-            throw new PluginException("Version properties could not be read!");
-        } catch (NumberFormatException exception) {
-            throw new PluginException("Version numbers could not be parsed!");
-        }
+	Properties properties = new Properties();
+	try {
+	    String propertiesPath = packageString.replace(".", "/");
+	    properties.load(getClass().getResourceAsStream("/" + propertiesPath + "/version.properties"));
+	    majorVersionNumber = Integer.parseInt(properties.getProperty("plugin.majorVersion"));
+	    logger.debug("Major version number: {}", majorVersionNumber);
+	    minorVersionNumber = Integer.parseInt(properties.getProperty("plugin.minorVersion"));
+	    logger.debug("Minor version number: {}", majorVersionNumber);
+	    buildVersionNumber = Integer.parseInt(properties.getProperty("plugin.buildVersion"));
+	    logger.debug("Build number: {}", majorVersionNumber);
+	    lastCommitDate = properties.getProperty("plugin.lastCommitDate");
+	    logger.debug("Last commit date: {}", majorVersionNumber);
+	    compileDateString = properties.getProperty("plugin.compileDate");
+	    logger.debug("Compile date: {}", majorVersionNumber);
+	    artifactVersionString = properties.getProperty("plugin.projectVersion");
+	    logger.debug("Artifact version: {}", majorVersionNumber);
+	} catch (IOException ex) {
+	    throw new PluginException("Version properties could not be read!", ex);
+	} catch (NumberFormatException ex) {
+	    throw new PluginException("Version numbers could not be parsed!", ex);
+	}
     }
 
     public String getName() {
-        return nameString;
+	return nameString;
     }
 
     public int getMajorVersionNumber() {
-        return majorVersionNumber;
+	return majorVersionNumber;
     }
 
     public int getMinorVersionNumber() {
-        return minorVersionNumber;
+	return minorVersionNumber;
     }
 
     public int getBuildVersionNumber() {
-        return buildVersionNumber;
+	return buildVersionNumber;
     }
 
     public String getCompileDate() {
-        return compileDateString;
+	return compileDateString;
     }
 
     public String getLastCommitDate() {
-        return lastCommitDate;
+	return lastCommitDate;
     }
 
     public String getArtifactVersion() {
-        return artifactVersionString;
+	return artifactVersionString;
     }
 
     public String getDescription() {
-        return descriptionString;
+	return descriptionString;
     }
 
     public boolean isMavenVersionCorrect() throws PluginException {
-        // this tests that the correct build number is specified in the pom.xml based on the current svn version
-        // either the correct build number or a snapshot version are valid
-        String errorMessage = "The maven version does not match either the snapshot nor the current svn build number.\n The pom.xml must be updated, please use either the correct build number or a snapshot version.";
-        String svnVersion = getMajorVersionNumber() + "." + getMinorVersionNumber() + "." + getBuildVersionNumber() + "-";
-        logger.debug("svnVersion: " + svnVersion + " ... ");
-        String snapshotVersion = getMajorVersionNumber() + "." + getMinorVersionNumber() + "-";
-        logger.debug("snapshotVersion: " + snapshotVersion + " ... " + "-SNAPSHOT");
-        String mavenBuildVersion = getArtifactVersion();
-        logger.debug("mavenBuildVersion: " + mavenBuildVersion);
-        if (mavenBuildVersion.endsWith("-SNAPSHOT")) {
-            if (mavenBuildVersion.startsWith(snapshotVersion)) {
-                return true;
-            } else {
-                throw new PluginException(errorMessage);
-            }
-        } else {
-            if (mavenBuildVersion.startsWith(svnVersion)) {
-                return true;
-            } else {
-                throw new PluginException(errorMessage);
-            }
-        }
+	// this tests that the correct build number is specified in the pom.xml based on the current svn version
+	// either the correct build number or a snapshot version are valid
+	final String errorMessage = "The maven version does not match either the snapshot nor the current svn build number.\n The pom.xml must be updated, please use either the correct build number or a snapshot version.";
+
+	final String svnVersion = getMajorVersionNumber() + "." + getMinorVersionNumber() + "." + getBuildVersionNumber() + "-";
+	// Printing to standard out for use in unit test!
+	System.out.println("svnVersion: " + svnVersion + " ... ");
+
+	final String snapshotVersion = getMajorVersionNumber() + "." + getMinorVersionNumber() + "-";
+	// Printing to standard out for use in unit test!
+	System.out.println("snapshotVersion: " + snapshotVersion + " ... " + "-SNAPSHOT");
+
+	final String mavenBuildVersion = getArtifactVersion();
+	// Printing to standard out for use in unit test!
+	System.out.println("mavenBuildVersion: " + mavenBuildVersion);
+
+	if (mavenBuildVersion.endsWith("-SNAPSHOT")) {
+	    if (mavenBuildVersion.startsWith(snapshotVersion)) {
+		return true;
+	    } else {
+		throw new PluginException(errorMessage);
+	    }
+	} else {
+	    if (mavenBuildVersion.startsWith(svnVersion)) {
+		return true;
+	    } else {
+		throw new PluginException(errorMessage);
+	    }
+	}
     }
 }
