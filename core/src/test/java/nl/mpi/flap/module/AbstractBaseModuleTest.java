eaf614b64c88d7e28e834427427b40315449a899
Peter Withers <Peter.Withers@mpi.nl>
2013-01-30 13:18:18 +0000

    Changed the BasePlugin name to BaseModule because this interface is used for both modules and for plugins. Separated out module code from plugin code in the package names.

--- core/src/test/java/nl/mpi/flap/module/AbstractBaseModuleTest.java
+++ core/src/test/java/nl/mpi/flap/module/AbstractBaseModuleTest.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package nl.mpi.kinnate.plugin;
+package nl.mpi.flap.module;
 
 import junit.framework.TestCase;
 import nl.mpi.flap.plugin.AbstractBaseModule;
